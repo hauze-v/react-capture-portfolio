b47a02a08e7616d1d2994d6b9b4021e8e56caa20
Adam Hauze <hauze_v@protonmail.com>
2020-12-13 09:17:07 -0500

    Styling the Services section!

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -4,62 +4,83 @@ import diaphragm from "../img/diaphragm.svg";
 import money from "../img/money.svg";
 import teamwork from "../img/teamwork.svg";
 import home2 from "../img/home2.png";
+// Import Styles
+import { Layout, Description, Image, Hide } from "../styles";
+import styled from "styled-components";
 
 const ServicesSection = () => {
   return (
-    <div className="services">
-      <div className="description">
+    <Services>
+      <Description>
         <h2>
           High <span>quality</span> services
         </h2>
-        <div className="cards">
-          <div className="card">
+        <Cards>
+          <Card>
             <div className="icon">
               <img src={clock} alt="clock icon" />
               <h3>Efficient</h3>
             </div>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam
-            nulla numquam rem aliquam velit fugit dignissimos quos rerum nam,
-            optio accusamus veritatis tempora eaque nemo neque magni debitis
-            iusto adipisci.
-          </div>
-          <div className="card">
+            <p>Lorem ipsum dolor, sit amet.</p>
+          </Card>
+          <Card>
             <div className="icon">
               <img src={teamwork} alt="teamwork icon" />
               <h3>Teamwork</h3>
             </div>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam
-            nulla numquam rem aliquam velit fugit dignissimos quos rerum nam,
-            optio accusamus veritatis tempora eaque nemo neque magni debitis
-            iusto adipisci.
-          </div>
-          <div className="card">
+            <p>Lorem ipsum dolor, sit amet..</p>
+          </Card>
+          <Card>
             <div className="icon">
               <img src={diaphragm} alt="diaphragm icon" />
               <h3>Diaphragm</h3>
             </div>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam
-            nulla numquam rem aliquam velit fugit dignissimos quos rerum nam,
-            optio accusamus veritatis tempora eaque nemo neque magni debitis
-            iusto adipisci.
-          </div>
-          <div className="card">
+            <p>Lorem ipsum dolor, sit amet..</p>
+          </Card>
+          <Card>
             <div className="icon">
               <img src={money} alt="money icon" />
               <h3>Affordable</h3>
             </div>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam
-            nulla numquam rem aliquam velit fugit dignissimos quos rerum nam,
-            optio accusamus veritatis tempora eaque nemo neque magni debitis
-            iusto adipisci.
-          </div>
-        </div>
-      </div>
-      <div className="image">
+            <p>Lorem ipsum dolor, sit amet..</p>
+          </Card>
+        </Cards>
+      </Description>
+      <Image>
         <img src={home2} alt="Expensive camera" />
-      </div>
-    </div>
+      </Image>
+    </Services>
   );
 };
+
+const Services = styled(Layout)`
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
+`;
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Card = styled.div`
+  flex-basis: 20rem;
+  .icon {
+    display: flex;
+    align-items: center;
+    h3 {
+      margin-left: 1rem;
+      background: white;
+      color: black;
+      padding: 1rem;
+      border-radius: 2px;
+    }
+  }
+`;
 
 export default ServicesSection;

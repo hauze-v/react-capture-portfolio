f9ac5cd8cce1abf0ca9cfced959edfc174967c2c
Adam Hauze <hauze_v@protonmail.com>
2020-12-14 09:39:19 -0500

    Implemented react-router-dom package to handle page navigation. Used Route and Switch components.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 // Import Pages
 import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 // GLobal Style
 import GlobalStyle from "./components/GlobalStyle";
+// Router
+import { Switch, Route } from "react-router-dom";
+import AboutSection from "./components/AboutSection";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route exact path="/" component={AboutUs}></Route>
+        <Route exact path="/work" component={OurWork}></Route>
+        <Route exact path="/contact" component={ContactUs}></Route>
+      </Switch>
     </div>
   );
 }
